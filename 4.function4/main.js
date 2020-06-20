08bfa05d9373fb37a3744e5b39df601aeef15bd4
olivia <wangol@oocl.com>
2020-06-20 14:08:54 +0800

    commit

--- 4.function4/main.js
+++ 4.function4/main.js
@@ -1,6 +1,6 @@
 function countWords(message){
     var array=message.split(' ');
-    var num=array.length();
+    var num=array.length;
     console.log(num);
   }
 countWords('Good morning, I love JavaScript.'); // should return 5
